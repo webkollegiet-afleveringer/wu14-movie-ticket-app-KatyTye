2535f465aa5e83c6bc62fc4e44a8785b1a8d7e89
David Majholt <d.boy.dragoz@gmail.com>
2026-04-16 09:11:08 +0200

    feat: Added router to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router"
+import Home from "./pages/Home"
+import Layout from "./Layout"
+
 function App() {
 
+	const BrowserRouter = createBrowserRouter([
+		{
+			id: "root",
+			element: <Layout />,
+			children: [
+				{
+					index: true,
+					element: <Home />
+				}
+			]
+		}
+	])
+
 	return (
-		<>
-		</>
+		<RouterProvider router={BrowserRouter} />
 	)
 }
 
