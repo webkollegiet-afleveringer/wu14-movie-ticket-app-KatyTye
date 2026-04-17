6bfb69563a2019330a3742d97919c1206160ae6c
David Majholt <d.boy.dragoz@gmail.com>
2026-04-17 10:35:53 +0200

    style: Added style for details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -6,38 +6,39 @@ export default function Details() {
 	const [showMore, setShowMore] = useState(false)
 	const movie = useRouteLoaderData("movie")
 
-	return (<main className="page-content">
-		<div className="page-content__images">
+	return (<main className="page-content details">
+		<div className="details__images">
 			{movie.images.map((name, idx) => {
 				return <img src={`${location.origin}/images/${name}.jpg`} alt={`image ${idx}`}
+					className="details__image"
 					key={`movies-images-${idx}`} />
 			})}
 		</div>
-		<article className="page-content__article">
-			<h2 className="page-content__article-title">
+		<article className="details__article">
+			<h2 className="details__article-title">
 				{movie.name}
 			</h2>
-			<p className="page-content__article-short-description">Director: <span>
+			<p className="details__article-short-description">Director: <span>
 				{movie.director}
 			</span> | <FaStar /> <span>
 					{movie.rated}
 				</span></p>
-			<ol className="page-content__article-tags">
-				<li className="page-content__article-tag">
-					{movie.tags[0]}
-				</li>
-				<li className="page-content__article-tag">
-					{movie.tags[1]}
-				</li>
-				<li className="page-content__article-tag">
+			<ol className="details__article-tags">
+				{movie.tags.map((tag, idx) => {
+					return <li className="details__article-tag"
+						key={`movie-tags-${idx}`}>
+						{tag}
+					</li>
+				})}
+				<li className="details__article-tag">
 					{movie.duration}
 				</li>
 			</ol>
-			<h3 className="page-content__article-title-disc">
+			<h3 className="details__article-title-disc">
 				Synopsis
 			</h3>
-			<p className="page-content__description">
-				<span className="page-content__description-text">
+			<p className="details__description">
+				<span className="details__description-text">
 					{movie.description.split(" ").map((txt, idx) => {
 						if (idx <= 18) {
 							return txt
@@ -47,14 +48,14 @@ export default function Details() {
 					}).join(" ")}
 				</span>
 				<span onClick={() => setShowMore(!showMore)}
-					className="page-content__description-button blue-text">
-					{(showMore == false && "Read More"
-						|| "Hide More"
+					className="details__description-button">
+					{(showMore == false && " Read More"
+						|| " Hide More"
 					)}
 				</span>
 			</p>
 		</article>
 		<Link to={`/seats/${movie.id}`}
-			className="page-content__button">Book Ticket</Link>
+			className="details__button">Book Ticket</Link>
 	</main>)
 }
