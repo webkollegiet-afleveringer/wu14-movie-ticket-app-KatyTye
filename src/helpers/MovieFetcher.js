6d28f00c74277521a7e2adceb6b4e2e9db57d06e
David Majholt <d.boy.dragoz@gmail.com>
2026-04-21 11:26:14 +0200

    feat: Added cinema to home page

--- src/helpers/MovieFetcher.js
+++ src/helpers/MovieFetcher.js
@@ -9,24 +9,38 @@ const fetchHeader = {
 	}
 }
 
+const getLocation = () => new Promise((resolve, reject) => {
+	navigator.geolocation.getCurrentPosition((position) => {
+		const { latitude, longitude } = position.coords
+		resolve({
+			"lat": latitude,
+			"lon": longitude
+		})
+	}, reject)
+})
+
 export async function initialMovieFetches() {
 	if (!import.meta.env.VITE_PRODUCTION) {
 
 	} else {
-		const [trendingRes, recommendationsRes, commingSoonRes] = await Promise.all([
+		const loc = await getLocation()
+		const [trendingRes, recommendationsRes, commingSoonRes, cinemaRes] = await Promise.all([
 			fetch(`${import.meta.env.VITE_API_URL}/trending/movie/week`, fetchHeader),
 			fetch(`${import.meta.env.VITE_API_URL}/movie/634649/recommendations`, fetchHeader),
-			fetch(`${import.meta.env.VITE_API_URL}/movie/upcoming`, fetchHeader)
+			fetch(`${import.meta.env.VITE_API_URL}/movie/upcoming`, fetchHeader),
+			fetch(`${import.meta.env.VITE_GEO_API_URL}&filter=circle:${loc.lon},${loc.lat},5000&bias=proximity:${loc.lon},${loc.lat}&limit=10&apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
 		])
 
 		const trending = await trendingRes.json()
 		const recommendations = await recommendationsRes.json()
 		const commingSoon = await commingSoonRes.json()
+		const cinema = await cinemaRes.json()
 
 		return {
 			trending: trending.results,
 			recommendations: recommendations.results,
-			commingSoon: commingSoon.results
+			commingSoon: commingSoon.results,
+			cinema: cinema.features
 		}
 	}
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { FaStar } from "react-icons/fa"
 import { Link, useRouteLoaderData } from "react-router"
 import { convertDateToText } from "../helpers/Converter"
+import { FaLocationDot } from "react-icons/fa6"
 
 export default function Home() {
 	const movieData = useRouteLoaderData("root")
@@ -9,13 +10,13 @@ export default function Home() {
 		<section className="home-content__soon">
 			<h2 className="home-content__soon-title">Coming Soon</h2>
 			<ol className="home-content__soon-list">
-				{movieData?.trending?.map((movie, index) => {
+				{movieData?.commingSoon?.map((movie, index) => {
 
 					if (index > 9) { return }
 
 					const releaseDate = new Date(movie.release_date)
 
-					return <li className="home-content__soon-item" key={`movie-trending-${index}`}>
+					return <li className="home-content__soon-item" key={`movie-commingSoon-${index}`}>
 						<Link to={`/details/${movie.id}`}>
 							<img className="home-content__soon-item-image"
 								loading="lazy" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt="image of movie" />
@@ -27,6 +28,41 @@ export default function Home() {
 					</li>
 				}) || <p>No movies where found that is comming soon</p>}
 			</ol>
+		</section>
+
+		<section className="home-content__cinema">
+			<h2 className="home-content__cinema-title">Cinema Near You</h2>
+			<ul className="home-content__cinema-list">
+				{movieData?.cinema?.map((cinema, index) => {
+					return <li className="home-content__cinema-item" key={`cinema-${index}`}>
+						<div className="home-content__cinema-item-div">
+							<p className="home-content__cinema-item-div-name">{cinema.properties.name.split(" ")[0]}</p>
+							<p className="home-content__cinema-item-div-special">cinema</p>
+						</div>
+						<div className="home-content__cinema-item-div">
+							<p className="home-content__cinema-item-loc">
+								<FaLocationDot className="home-content__cinema-item-loc-img" />
+								<span className="home-content__cinema-item-loc-amt">
+									{(cinema.properties.distance / 1000).toFixed(2)} Kilometers
+								</span>
+							</p>
+							<p className="home-content__cinema-item-name">
+								{cinema.properties.name}
+							</p>
+							<p className="home-content__cinema-item-closing">
+								Closed 10.00 PM
+							</p>
+						</div>
+
+						<p className="home-content__cinema-item-stars">
+							<FaStar className="home-content__cinema-item-star" />
+							<span className="home-content__cinema-item-stars-amt">
+								{((Math.random() * 10) / 2).toFixed(1)}
+							</span>
+						</p>
+					</li>
+				})}
+			</ul>
 		</section>
 	</main>)
 }
