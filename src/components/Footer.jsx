bafe0b12445508221eec15cc9b4eeee19973dcc1
David Majholt <d.boy.dragoz@gmail.com>
2026-04-23 14:03:27 +0200

    fix: Removed footer on pages that its not in from design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,29 @@
 import { BsFillBookmarkDashFill } from "react-icons/bs";
+import { NavLink, useLocation } from "react-router";
 import { BiSolidUser } from "react-icons/bi";
 import { PiHouseFill } from "react-icons/pi";
 import { FaCompass } from "react-icons/fa";
-import { NavLink } from "react-router";
 
 export default function Footer() {
+	const urlPath = useLocation()?.pathname
 
-	return (<footer className="bottom-content">
-		<NavLink className={"bottom-content__link"} to={"/"}>
-			<PiHouseFill className="bottom-content__link-icon" />
-		</NavLink>
+	return ((urlPath == "/" || urlPath.includes("explore") || urlPath.includes("explore")
+		|| urlPath.includes("settings") || urlPath.includes("plan")) &&
+		<footer className="bottom-content">
+			<NavLink className={"bottom-content__link"} to={"/"}>
+				<PiHouseFill className="bottom-content__link-icon" />
+			</NavLink>
 
-		<NavLink className={"bottom-content__link"} to={"/explore"}>
-			<FaCompass className="bottom-content__link-icon" />
-		</NavLink>
+			<NavLink className={"bottom-content__link"} to={"/explore"}>
+				<FaCompass className="bottom-content__link-icon" />
+			</NavLink>
 
-		<NavLink className={"bottom-content__link"} to={"/plan"}>
-			<BsFillBookmarkDashFill className="bottom-content__link-icon" />
-		</NavLink>
+			<NavLink className={"bottom-content__link"} to={"/plan"}>
+				<BsFillBookmarkDashFill className="bottom-content__link-icon" />
+			</NavLink>
 
-		<NavLink className={"bottom-content__link"} to={"/profile"}>
-			<BiSolidUser className="bottom-content__link-icon" />
-		</NavLink>
-	</footer>)
+			<NavLink className={"bottom-content__link"} to={"/profile"}>
+				<BiSolidUser className="bottom-content__link-icon" />
+			</NavLink>
+		</footer> || <></>)
 }
